ff5b1105f2f2efd673ace6ed98b787b484c66e53
Luana Palmeira <luana.lp29@outlook.com>
2025-04-30 02:55:34 -0300

    Conectando o Front com o Back (Conexão para o funcionamento da tela de login)

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,12 +1,36 @@
 import { useNavigate } from "react-router-dom";
 import Logo from "../../assets/Tela Login/logo-ws.png";
 import styles from "./Login.module.scss";
+import api from "../../Services/Api.jsx";
+import { useState } from "react";
 
 function Login() {
     const navigate = useNavigate() //Iniciando o hook useNavigate
 
     const irParaReembolsos = () => {
         navigate("/reembolsos")  //Redirecionando para a página de reembolsos
+    }
+
+    //Iniciando os estados
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+
+    const fazerLogin = async (e) => {
+        e.preventDefault()
+
+        try {
+            const resposta = await api.post("/colaborador/login", {
+                "email": email, 
+                "senha": senha
+            })
+            console.log(resposta.data)
+            alert("Login realizado com sucesso!")
+            irParaReembolsos()  // <--- Redirecionamento
+
+        } catch (error) {
+            console.log("Erro ao fazer o Login", error)
+            alert("Erro no Login aqui ó!")
+        }
     }
 
     return (
@@ -20,13 +44,13 @@ function Login() {
                     <p>Sistema de Emissão de Boletos e Parcelamento</p>
 
                     <form action="">
-                        <input type="email" name="email" id="email" placeholder="Email" />
-                        <input type="password" name="senha" id="senha" placeholder="Senha" />
+                        <input type="email" name="email" id="email" placeholder="Email" value = {email} onChange = {(e) => setEmail(e.target.value)} />
+                        <input type="password" name="senha" id="senha" placeholder="Senha" value = {senha} onChange = {(e) => setSenha(e.target.value)} />
 
                         <a href="">Esqueci minha senha</a>
 
                         <div>
-                            <button className={styles.botaoEntrar} onClick={irParaReembolsos} >Entrar</button>
+                            <button className={styles.botaoEntrar} onClick={fazerLogin}>Entrar</button>
                             <button className={styles.botaoCriar}>Criar conta</button>
                         </div>
                     </form>
