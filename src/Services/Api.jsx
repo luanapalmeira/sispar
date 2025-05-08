42ea1a2f494006669bbcb3c4e46b4fbb96792967
Luana Palmeira <luana.lp29@outlook.com>
2025-05-07 23:12:59 -0300

    Troca de váriavel

--- src/Services/Api.jsx
+++ src/Services/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";  //Importa a biblioteca axios, que é usada para faze
 
 //Cria uma instância do axios, chamada "api".
 const api = axios.create({
-    baseURL: "http://localhost:5000"  //Define a URL base para todas as requisições feitas com essa instância.
+    baseURL: "https://sispar-back.onrender.com"  //Define a URL base para todas as requisições feitas com essa instância.
 })
 
 export default api;
